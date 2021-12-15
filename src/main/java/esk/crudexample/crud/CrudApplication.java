148cfd5f630ea54eb4d6a6c3150f77bbdede18b0
= <=>
2021-12-16 00:06:01 +0300

    updated model and root response

--- src/main/java/esk/crudexample/crud/CrudApplication.java
+++ src/main/java/esk/crudexample/crud/CrudApplication.java
@@ -1,11 +1,15 @@
 package esk.crudexample.crud;
 
+import java.time.LocalDate;
+import java.time.Month;
 import java.util.List;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import esk.crudexample.crud.esk.crudexample.crud.student.Student;
 
 @SpringBootApplication
 @RestController
@@ -16,7 +20,8 @@ public class CrudApplication {
 	}
 
 	@GetMapping
-	public List<String> hello() {
-		return List.of("Hello", "World");
+	public List<Student> hello() {
+		return List.of(
+				new Student(1L, "John Doe", "john@test.com", LocalDate.of(2000, Month.JANUARY, 5), 21));
 	}
 }
